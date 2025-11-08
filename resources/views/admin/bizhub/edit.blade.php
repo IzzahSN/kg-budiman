7422dbad509514019213339037920ed010140209
IzzahSN <izzahfsn@gmail.com>
2025-11-08 14:30:13 +0800

    `Updated admin.bizhub.create and edit views with new layout and form fields`

--- resources/views/admin/bizhub/edit.blade.php
+++ resources/views/admin/bizhub/edit.blade.php
@@ -1,3 +1,46 @@
 <x-admin-layout :title="'Budiman Biz Hub'">
-    
+    <!-- Tambah padding menyeluruh (kiri, kanan, atas, bawah) -->
+    <div class="p-4 sm:p-6 lg:p-8">
+        <div class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800">
+            <div class="flex items-center justify-between mb-4">
+                <h2 class="text-lg font-semibold text-gray-900 dark:text-gray-100">Kemaskini Perniagaan</h2>
+                
+                {{-- button kembali ke route admin.ajk.index --}}
+            </div>
+            {{-- form input ajk details [gambar, nama ajk, jawatan, no. telefon] --}}
+            <form action="{{ route('admin.bizhub.store') }}" method="POST" enctype="multipart/form-data" class="space-y-6">
+                @csrf
+                
+                <div>
+                    <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nama Perniagaan</label>
+                    <input type="text" name="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-dark dark:focus:border-primary-dark" required>
+                </div>
+
+                <div>
+                    <label for="jawatan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Keterangan</label>
+                    <input type="text" name="jawatan" id="jawatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-dark dark:focus:border-primary-dark" required>
+                </div>
+
+                <div>
+                    <label for="no_telefon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">No. Telefon</label>
+                    <input type="text" name="no_telefon" id="no_telefon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-dark dark:focus:border-primary-dark" required>
+                </div>
+
+                <div>
+                    <label for="gambar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Gambar Perniagaan</label>
+                    <input type="file" name="gambar" id="gambar" accept="image/*" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary
+                    focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-dark dark:focus:border-primary-dark" required>
+                    <div class="mt-1 text-sm text-gray-500 dark:text-gray-300">Format yang disokong: JPG, JPEG, PNG. Saiz maksimum: 2MB.</div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <a href="{{ route('admin.bizhub.index') }}" class="text-sm text-gray-600 dark:text-gray-400 hover:underline">Kembali ke Senarai</a>
+                    
+                    <button type="submit" class="px-4 py-2 bg-primary text-white rounded-lg shadow hover:bg-primary-dark transition duration-300 transform hover:scale-[1.02]">Simpan Ahli Jawatankuasa</button>
+                </div>
+            </form>
+
+        </div>
+
+    </div>
 </x-admin-layout>
